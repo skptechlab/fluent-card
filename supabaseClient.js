cc67cc09fb617864db1de45d6127a5ba63819905
masqMaster <masqueradessol@outlook.com>
2025-05-29 22:06:51 +0200

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,13 +1,3 @@
-// // supabaseClient.js
-// import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
-
-// // Replace these with your actual values from Supabase dashboard
-// const SUPABASE_URL = "https://pklmlttcycefshwxqcwu.supabase.co";
-// const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBrbG1sdHRjeWNlZnNod3hxY3d1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMxMDE0OTQsImV4cCI6MjA1ODY3NzQ5NH0.1NxMlj8YvWHpntGXRlXjB4ntxW5aO-uJH8USLvlGm4Y";
-
-// export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-
-
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
 let supabase = null;
